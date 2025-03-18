d0b3e3eb4e08c47ca2e64dde5a3864b1cbeff7c9
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-03-18 06:57:51 -0300

    autenticação

--- 37 - Autenticação no Adonis/desafio_node_adonis/start/routes.ts
+++ 37 - Autenticação no Adonis/desafio_node_adonis/start/routes.ts
@@ -20,20 +20,33 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', 'HomeController.index')
-Route.get('/clientes', 'ClientesController.index').as('clientes.index')
-Route.get('/clientes/novo', 'ClientesController.create')
-Route.post('/clientes', 'ClientesController.store')
-Route.get('/clientes/:id', 'ClientesController.show')
-Route.get('/clientes/:id/edit', 'ClientesController.edit')
-Route.post('/clientes/multiple-delete', 'ClientesController.destroyMultiple')
-Route.post('/clientes/:id', 'ClientesController.update')
-Route.get('/clientes/:id/delete', 'ClientesController.destroy')
+// Rotas públicas
+Route.get('/login', 'AuthController.showLogin').as('auth.login')
+Route.post('/login', 'AuthController.login')
 
-Route.get('/administradores', 'AdministradoresController.index').as('administradores.index')
-Route.get('/administradores/novo', 'AdministradoresController.create')
-Route.post('/administradores', 'AdministradoresController.store')
-Route.get('/administradores/:id', 'AdministradoresController.show')
-Route.get('/administradores/:id/edit', 'AdministradoresController.edit')
-Route.post('/administradores/:id', 'AdministradoresController.update')
-Route.get('/administradores/:id/delete', 'AdministradoresController.destroy')
+// Grupo de rotas protegidas
+Route.group(() => {
+  Route.get('/', 'HomeController.index')
+  
+  // Rotas de clientes
+  Route.get('/clientes', 'ClientesController.index').as('clientes.index')
+  Route.get('/clientes/novo', 'ClientesController.create')
+  Route.post('/clientes', 'ClientesController.store')
+  Route.get('/clientes/:id', 'ClientesController.show')
+  Route.get('/clientes/:id/edit', 'ClientesController.edit')
+  Route.post('/clientes/multiple-delete', 'ClientesController.destroyMultiple')
+  Route.post('/clientes/:id', 'ClientesController.update')
+  Route.get('/clientes/:id/delete', 'ClientesController.destroy')
+
+  // Rotas de administradores
+  Route.get('/administradores', 'AdministradoresController.index').as('administradores.index')
+  Route.get('/administradores/novo', 'AdministradoresController.create')
+  Route.post('/administradores', 'AdministradoresController.store')
+  Route.get('/administradores/:id', 'AdministradoresController.show')
+  Route.get('/administradores/:id/edit', 'AdministradoresController.edit')
+  Route.post('/administradores/:id', 'AdministradoresController.update')
+  Route.get('/administradores/:id/delete', 'AdministradoresController.destroy')
+
+  // Rota de logout
+  Route.post('/logout', 'AuthController.logout').as('auth.logout')
+}).middleware('auth')
